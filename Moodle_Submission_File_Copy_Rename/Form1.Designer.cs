b97f7f2664d1d43b25cecf6a47946918f7d3c0f8
坂口 祐稀 <s1712033@s.do-johodai.ac.jp>
2020-08-01 15:34:02 +0900

    Ver.2 更新

---
+++ Moodle_Submission_File_Copy_Rename/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Moodle_Submission_File_Copy_Rename
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.doing_check = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(174, 30);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "フォルダ選択";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // doing_check
+            // 
+            this.doing_check.Enabled = false;
+            this.doing_check.Location = new System.Drawing.Point(174, 140);
+            this.doing_check.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.doing_check.Name = "doing_check";
+            this.doing_check.Size = new System.Drawing.Size(112, 28);
+            this.doing_check.TabIndex = 2;
+            this.doing_check.Text = "コピー開始";
+            this.doing_check.UseVisualStyleBackColor = true;
+            this.doing_check.Click += new System.EventHandler(this.doing_check_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 12);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(376, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Moodleからダウンロードし、解凍したフォルダーを選択してください";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 180);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.doing_check);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximumSize = new System.Drawing.Size(485, 227);
+            this.MinimumSize = new System.Drawing.Size(485, 227);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "提出ファイルデータコピーリングシステム";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button doing_check;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Moodle_Submission_File_Copy_Rename/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Moodle_Submission_File_Copy_Rename
+{
+    public partial class Form1 : Form
+    {
+        string Folder_path = "", extension = "";
+        int Try_Parse = 0, timer_count = 0, Success = 0, Failed = 0;
+        public Form1()
+        {
+            MaximizeBox = false;
+            InitializeComponent();
+        }
+        private void doing_check_Click(object sender, EventArgs e)
+        {
+            DialogResult Ques = MessageBox.Show("確認してください!" + "\n" + "現バージョンで行えるファイルのコピーは各学生提出フォルダにつき1つだけです!" + "\n" + "各学生が提出したファイルは1つだけですか?", "CHECK!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+            if (Ques == DialogResult.Yes)
+            {
+                DialogResult check = MessageBox.Show("解凍された & ファイルコピー先のフォルダ: " + Folder_path + "\n\n以上の設定でデータコピーを行います。\n(同じファイル名, 拡張子名のファイルは上書きされます。)\n\nよろしいですか?", "確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (check == DialogResult.Yes)
+                    Copy_Data();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "フォルダを指定してください。";
+            fbd.ShowNewFolderButton = false;
+            if (fbd.ShowDialog(this) == DialogResult.OK)
+            {
+                Folder_path = fbd.SelectedPath;
+                label1.Text = Folder_path;
+                doing_check.Enabled = true;
+            }
+        }
+        private void Copy_Data()
+        {
+            string[] subFolders = System.IO.Directory.GetDirectories(@Folder_path, "*", System.IO.SearchOption.AllDirectories);
+            for (int i = 0; i < subFolders.Length; i++)
+            {
+                try
+                {
+                    string do_FOLDER = subFolders[i];
+                    string[] file_URI = System.IO.Directory.GetFiles(@subFolders[i], "*", System.IO.SearchOption.AllDirectories);
+                    bool all_check = true;
+                    for (int j = 0; j <= 7; j++)
+                    {
+                        if (j == 7)
+                        {
+                            string space = subFolders[i].Substring(subFolders[i].LastIndexOf("\\") + 1 + j, 1);
+                            if (space != " ")
+                                all_check = false;
+                        }
+                        else
+                        {
+                            bool parse_check = int.TryParse(subFolders[i].Substring(subFolders[i].LastIndexOf("\\") + 1 + j, 1), out Try_Parse);
+                            if (parse_check == false)
+                                all_check = false;
+                        }
+                    }
+                    if (all_check)
+                    {
+                        int student_number = int.Parse(subFolders[i].Substring(subFolders[i].LastIndexOf("\\") + 1, 7));
+                        extension = file_URI[0].Substring(file_URI[0].LastIndexOf("."));
+                        string COPY_File_URI = Folder_path + "\\" + student_number + extension;
+                        System.IO.File.Copy(file_URI[0], @COPY_File_URI, true);
+                        Success++;
+                    }
+                    else
+                        Copy_Failed(i + 1, subFolders[i]);
+                }
+                catch
+                {
+                    Copy_Failed(i + 1, subFolders[i]);
+                }
+            }
+            MessageBox.Show("ファイルコピーが完了しました。\n" +
+                "成功: " + Success + "件\n" +
+                "失敗: " + Failed + "件\n", "Finish", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Close();
+        }
+        private void Copy_Failed(int i, string ERROR_FOLDER)
+        {
+            ERROR_FOLDER = ERROR_FOLDER.Substring(Folder_path.Length + 1);
+            MessageBox.Show(i + "個目のフォルダで問題が発生したため、ファイルコピーが行えませんでした。\nフォルダ名などを確認してください。\nフォルダ名の先頭が学籍番号7桁の数字になっているか, 下記フォルダの中に保存されているファイル拡張子が \".\"で区切られていることを確認してください。\n"
+                + "\n" + "エラー発生フォルダ名: " + ERROR_FOLDER, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Failed++;
+        }
+    }
+}
