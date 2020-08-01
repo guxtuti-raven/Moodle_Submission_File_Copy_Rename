2e2bb34f62c3a301ffee156a7aad236cc46dd2d8
坂口 祐稀 <s1712033@s.do-johodai.ac.jp>
2020-08-01 16:51:41 +0900

    コメントアウト書き込み

--- Moodle_Submission_File_Copy_Rename/Form1.cs
+++ Moodle_Submission_File_Copy_Rename/Form1.cs
@@ -5,30 +5,54 @@ namespace Moodle_Submission_File_Copy_Rename
 {
     public partial class Form1 : Form
     {
-        string Folder_path = "", extension = "";
-        int Try_Parse = 0, timer_count = 0, Success = 0, Failed = 0;
+        /* 変数情報
+         * 変数名: Folder_path
+         * 型名: string(文字)型
+         * 内容: 指定されたフォルダURI(コピー先フォルダ)
+         * 
+         * 変数名: Try_Parse
+         * 型名: int(整数)型
+         * 内容: int.TryParseメソッド out用
+         * 
+         * 変数名: Success
+         * 型名: int(整数)型
+         * 内容: エラー無し(コピー完了)ファイル個数
+         * 
+         * 変数名: Failed
+         * 型名: int(整数)型
+         * 内容: エラー発生(コピー不可)ファイル個数
+         */
+
+        string Folder_path = "";
+        int Try_Parse = 0, Success = 0, Failed = 0;
         public Form1()
         {
+            //最大化禁止
             MaximizeBox = false;
+            //初期化処理?
             InitializeComponent();
         }
         private void doing_check_Click(object sender, EventArgs e)
         {
+            //動作前ユーザー確認
             DialogResult Ques = MessageBox.Show("確認してください!" + "\n" + "現バージョンで行えるファイルのコピーは各学生提出フォルダにつき1つだけです!" + "\n" + "各学生が提出したファイルは1つだけですか?", "CHECK!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
             if (Ques == DialogResult.Yes)
             {
                 DialogResult check = MessageBox.Show("解凍された & ファイルコピー先のフォルダ: " + Folder_path + "\n\n以上の設定でデータコピーを行います。\n(同じファイル名, 拡張子名のファイルは上書きされます。)\n\nよろしいですか?", "確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                 if (check == DialogResult.Yes)
+                    //コピー用メソッドへ
                     Copy_Data();
             }
         }
         private void button1_Click(object sender, EventArgs e)
         {
+            //コピー先フォルダ指定
             FolderBrowserDialog fbd = new FolderBrowserDialog();
             fbd.Description = "フォルダを指定してください。";
             fbd.ShowNewFolderButton = false;
             if (fbd.ShowDialog(this) == DialogResult.OK)
             {
+                //ダイアログ"OK"クリック時
                 Folder_path = fbd.SelectedPath;
                 label1.Text = Folder_path;
                 doing_check.Enabled = true;
@@ -36,22 +60,44 @@ namespace Moodle_Submission_File_Copy_Rename
         }
         private void Copy_Data()
         {
+            //ファイルコピー用メソッド
+            /* 変数情報
+             * 変数名: subFolders
+             * 型名: string(文字)型配列
+             * 内容: 指定されたフォルダに格納されているフォルダURI配列
+             */
             string[] subFolders = System.IO.Directory.GetDirectories(@Folder_path, "*", System.IO.SearchOption.AllDirectories);
             for (int i = 0; i < subFolders.Length; i++)
             {
                 try
                 {
+                    // Exception(例外)発生するまで
+                    /* 変数情報
+                     * 変数名: do_FOLDER
+                     * 型名: string(文字)型
+                     * 内容: 動作実行中のフォルダURIを保持
+                     * 
+                     * 変数名: file_URI
+                     * 型名: string(文字)型配列
+                     * 内容: 動作フォルダ内に格納されているファイル配列
+                     * 
+                     * 変数名: all_check
+                     * 型名: bool(論理)型
+                     * 内容: ファイル名エラーチェック結果保持
+                     */
                     string do_FOLDER = subFolders[i];
                     string[] file_URI = System.IO.Directory.GetFiles(@subFolders[i], "*", System.IO.SearchOption.AllDirectories);
                     bool all_check = true;
+                    //ファイル名エラーチェック
                     for (int j = 0; j <= 7; j++)
                     {
+                        //8文字目がスペースか
                         if (j == 7)
                         {
-                            string space = subFolders[i].Substring(subFolders[i].LastIndexOf("\\") + 1 + j, 1);
-                            if (space != " ")
+                            if (subFolders[i].Substring(subFolders[i].LastIndexOf("\\") + 1 + j, 1) != " ")
                                 all_check = false;
                         }
+                        //1~7文字目が数字か
                         else
                         {
                             bool parse_check = int.TryParse(subFolders[i].Substring(subFolders[i].LastIndexOf("\\") + 1 + j, 1), out Try_Parse);
@@ -62,7 +108,7 @@ namespace Moodle_Submission_File_Copy_Rename
                     if (all_check)
                     {
                         int student_number = int.Parse(subFolders[i].Substring(subFolders[i].LastIndexOf("\\") + 1, 7));
-                        extension = file_URI[0].Substring(file_URI[0].LastIndexOf("."));
+                        string extension = file_URI[0].Substring(file_URI[0].LastIndexOf("."));
                         string COPY_File_URI = Folder_path + "\\" + student_number + extension;
                         System.IO.File.Copy(file_URI[0], @COPY_File_URI, true);
                         Success++;
@@ -72,6 +118,7 @@ namespace Moodle_Submission_File_Copy_Rename
                 }
                 catch
                 {
+                    //Exception発生時
                     Copy_Failed(i + 1, subFolders[i]);
                 }
             }
